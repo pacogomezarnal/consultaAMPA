27f1b4496b3304f1ee3c3775a3eb0a5c438d1aa4
Francisco José Gómez Arnal <fjgomez@florida-uni.es>
2018-05-25 00:30:11 +0200

    Aumento de fuentes

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   </head>
   <body>
     <div class="w3-row w3-padding-24 w3-margin-right w3-margin-left">
-      <div class="w3-card-4">
+      <div class="w3-card-4 w3-xlarge">
         <header class="w3-container w3-blue">
           <h4>Necessitem saber la vostra opinió respecte a la proposta de canvi d’horari, per part del Claustre de Professorat. Per tant, vos convoquem a que l’expresseu a través d’una consulta no vinculant, per orientar als representants dels pares/mares al Consell Escolar.</h4>
         </header>
@@ -21,7 +21,7 @@
           }
           ?>
         </header>
-        <div class="w3-container">
+        <div class="w3-container w3-xlarge">
           <form class="w3-container" action="votar.php" method="post">
             <p>
               <label class="w3-text-grey">Introduce tu DNI con la letra</label>
