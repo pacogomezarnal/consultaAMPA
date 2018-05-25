6d9a51642ea247a8a75b3f842e511aca844d080b
Francisco José Gómez Arnal <fjgomez@florida-uni.es>
2018-05-25 08:02:39 +0200

    Movil

--- index.php
+++ index.php
@@ -10,14 +10,14 @@
   </head>
   <body>
     <header class="w3-top w3-bar w3-theme w3-center">
-      <h1 class="w3-bar-item w3-center">Consulta no vinculant</h1>
+      <h2 class="w3-bar-item w3-center">Consulta no vinculant</h2>
     </header>
     <div class="w3-container w3-center" style="margin-top:90px">
       <hr>
       <h1>EL DILLUNS 28 DE MAIG DE 09:00 a 17:00 podeu expresar la vostra opinió telemàticament </h1>
     </div>
     <footer class="w3-container w3-bottom w3-theme w3-margin-top w3-center">
-      <img  class="" src="img/logo_ampa.jpg" alt="AMPA" width="25%">
+      <img  class="" src="img/logo_ampa.jpg" alt="AMPA" width="100%">
     </footer>
   </body>
 </html>
