8153441f893abd55d9fcf69e233e97be3aef72cb
Francisco José Gómez Arnal <fjgomez@florida-uni.es>
2018-05-28 00:10:09 +0200

    Cambio a formato movil

--- votar.php
+++ votar.php
@@ -29,31 +29,32 @@ if($incorrecto){
   <head>
     <meta charset="utf-8">
     <title>Consulta Horario AMPA Platero y yo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3mobile.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue.css">
   </head>
   <body>
-    <div class="w3-row w3-padding-24 w3-margin-right w3-margin-left">
-      <div class="w3-card-4">
-        <header class="w3-container w3-blue">
-          <h1>DNI: <?=$dni?></h1>
-        </header>
-        <div class="w3-container">
-          <form class="w3-color-container w3-xxxlarge" action="final.php" method="post">
-            <p>
-              <label class="w3-text-black">Horari proposat: els matins es mantenen igual sols es modifica l’horari de les vesprades de dilluns a dijous de 15:00 a 16:30.</label>
-              <br>
-              <input class="radio" type="radio" name="eleccion" value="si"> SI <br>
-              <input class="radio" type="radio" name="eleccion" value="no"> NO <br>
-              <input type="hidden" name="dni" value="<?=$dni?>">
-            </p>
-            <p><input type="submit" class="w3-btn w3-padding w3-blue" value="OPINAR"></p>
-          </form></p>
-        </div>
-        <footer class="w3-container w3-blue w3-center">
-          <img src="img/logo_ampa.jpg" alt="AMPA" width="25%">
-        </footer>
-      </div>
+    <header class="w3-top w3-bar w3-theme w3-center">
+      <h2 class="w3-bar-item w3-center w3-red">DNI: <?=$dni?></h2>
+    </header>
+    <div class="w3-container w3-center" style="margin-top:90px">
+      <p>Necessitem saber la vostra opinió respecte a la proposta de canvi d’horari, per part del Claustre de Professorat. Per tant, vos convoquem a que l’expresseu a través d’una consulta no vinculant, per orientar als representants dels pares/mares al Consell Escolar.</p>
     </div>
+    <div class="w3-container">
+      <form  action="final.php" method="post">
+        <p>
+          <label class="w3-text-black">Horari proposat: els matins es mantenen igual sols es modifica l’horari de les vesprades de dilluns a dijous de 15:00 a 16:30.</label>
+          <br>
+          <input type="radio" name="eleccion" value="si"> SI <br>
+          <input type="radio" name="eleccion" value="no"> NO <br>
+          <input type="hidden" name="dni" value="<?=$dni?>">
+        </p>
+        <p><input type="submit" class="w3-btn w3-padding w3-blue" value="OPINAR"></p>
+      </form>
+    </div>
+    <footer class="w3-container w3-bottom w3-theme w3-margin-top w3-center">
+      <img  class="" src="img/logo_ampa.jpg" alt="AMPA" width="100%">
+    </footer>
   </body>
 </html>
