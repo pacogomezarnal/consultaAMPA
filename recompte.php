d49dbac643e8ed9b11d00d174d84cc6b61a629ab
Francisco José Gómez Arnal <fjgomez@florida-uni.es>
2018-05-28 00:43:54 +0200

    Recompte

---
+++ recompte.php
@@ -0,0 +1,31 @@
+<?php
+//Recuento
+$resultadoSI=$mysqli->query("SELECT count(voto) as si from padres where voto='si'");
+$votoSI=$resultadoSI->fetch_assoc();
+$resultadoNO=$mysqli->query("SELECT  count(voto) as no from padres where voto='no'");
+$votoNO=$resultadoNO->fetch_assoc();
+?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Consulta Horario AMPA Platero y yo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3mobile.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue.css">
+    <link rel="stylesheet" href="/css/estilos.css">
+  </head>
+  <body>
+    <header class="w3-top w3-bar w3-theme w3-center">
+      <h2 class="w3-bar-item w3-center">Recompte</h2>
+    </header>
+    <div class="w3-container" style="margin-top:120px">
+      <h2>SI: <?=$votoSI?></h2>
+      <h2>NO: <?=$votoNO?></h2>
+    </div>
+    <footer class="w3-container w3-bottom w3-theme w3-margin-top w3-center">
+      <img  class="" src="img/logo_ampa.jpg" alt="AMPA" width="100%">
+    </footer>
+  </body>
+</html>
