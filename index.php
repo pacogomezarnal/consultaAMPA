97943ee39c346cc040b25820725c733d77c3feeb
Francisco José Gómez Arnal <fjgomez@florida-uni.es>
2018-05-28 19:28:28 +0200

    Recuento

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+$incorrecto=true;
+$mensaje="";
+//Recuento
+$mysqli = new mysqli("localhost", "root", "", "ampaplatero");
+if ($mysqli->connect_errno) {
+    $mensaje="En estos momentos no podemos almacenar tu voto. Prueba en 5 minutos";
+}else{
+  $resultadoSI=$mysqli->query("SELECT count(voto) as si from padres where voto='si'");
+  $votoSI=$resultadoSI->fetch_assoc();
+  $resultadoNO=$mysqli->query("SELECT  count(voto) as no from padres where voto='no'");
+  $votoNO=$resultadoNO->fetch_assoc();
+  $incorrecto=false;
+}
+if($incorrecto){
+  header('Location: index.php?m='.$mensaje);
+}
+?>
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
   <head>
@@ -7,33 +25,20 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3mobile.css">
     <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue.css">
+    <link rel="stylesheet" href="/css/estilos.css">
   </head>
   <body>
     <header class="w3-top w3-bar w3-theme w3-center">
-
-      <?php
-      if(isset($_GET["m"])){
-      ?>
-      <h2 class="w3-bar-item w3-center w3-red"><?=$_GET["m"]?></h2>
-      <?php
-      }else{
-      ?>
-      <h2 class="w3-bar-item w3-center">Consulta no vinculant</h2>
-      <?php
-      }
-      ?>
+      <h2 class="w3-bar-item w3-center">Recompte</h2>
     </header>
-    <div class="w3-container w3-center" style="margin-top:90px">
-      <p>Necessitem saber la vostra opinió respecte a la proposta de canvi d’horari, per part del Claustre de Professorat. Per tant, vos convoquem a que l’expresseu a través d’una consulta no vinculant, per orientar als representants dels pares/mares al Consell Escolar.</p>
-    </div>
-    <div class="w3-container">
-      <form  action="votar.php" method="post">
-        <p>
-          <label class="w3-text-grey">Introduïx el teu DNI amb la lletra</label>
-          <input class="w3-input w3-border" type="text" required="" name="dni">
-        </p>
-        <p><input type="submit" class="w3-btn w3-padding w3-blue" value="SEGÜENT"></p>
-      </form>
+    <div class="w3-container" style="margin-top:120px">
+      <?php
+      $total=$votoSI["si"]+$votoNO["no"];
+      $percSI=(100*$votoSI["si"])/$total;
+      $percNO=100-$percSI;
+      ?>
+      <h2>SI: <?php echo $votoSI['si']." (".round($percSI,2)."% dels que han opinat)";?></h2>
+      <h2>NO: <?php echo $votoNO['no']." (".round($percNO,2)."% dels que han opinat)";?></h2>
     </div>
     <footer class="w3-container w3-bottom w3-theme w3-margin-top w3-center">
       <img  class="" src="img/logo_ampa.jpg" alt="AMPA" width="100%">
